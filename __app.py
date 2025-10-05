af4f530ba915dbca46ad3306bc7488dd9e183d77
ertyuiopki10 <ejczupurek@gmail.com>
2025-10-05 21:47:36 +0200

    MAKAC TY GEJU CZEMU USTAWILES PORT 5000 JAK WINDOWS BLOKUJE TEN PORT

--- __app.py
+++ __app.py
@@ -248,6 +248,6 @@ def clear_logs():
 
 if __name__ == '__main__':
     print("🛠️ Uruchamianie Rzemieślnik OIOIOI API Server (Flask)...")
-    print("🌐 Otwórz: http://127.0.0.1:5000")
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    print("🌐 Otwórz: http://127.0.0.1:4000")
+    app.run(debug=True, host='0.0.0.0', port=4000)
     
